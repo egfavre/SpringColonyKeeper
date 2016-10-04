12628bb0f73bd188f5b4a6168f083ac23f7bc536
Liz Favre <egfavre@gmail.com>
2016-10-04 10:37:25 -0400

    added buttons to front page for login and register

--- src/main/java/com/egfavre/controllers/RestController.java
+++ src/main/java/com/egfavre/controllers/RestController.java
@@ -1,6 +1,8 @@
 package com.egfavre.controllers;
 
 import org.h2.tools.Server;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,17 +13,18 @@ import java.sql.SQLException;
 /**
  * Created by user on 10/4/16.
  */
-@org.springframework.web.bind.annotation.RestController
+@Controller
 public class RestController {
     @PostConstruct
     public void init() throws SQLException, FileNotFoundException {
         Server.createWebServer().start();
     }
 
-    @RequestMapping (path = "/", method = RequestMethod.GET)
-    public String displayWelcomePage (){
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String welcome () {
         return "welcome";
     }
+
 
 }
 
