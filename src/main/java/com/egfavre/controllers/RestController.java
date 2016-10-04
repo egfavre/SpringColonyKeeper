0d442e3d3f64cf663e4dc9b9fbe5e7956971c736
Liz Favre <egfavre@gmail.com>
2016-10-04 10:12:59 -0400

    displays localhost:8080

--- src/main/java/com/egfavre/controllers/RestController.java
+++ src/main/java/com/egfavre/controllers/RestController.java
@@ -1,6 +1,8 @@
 package com.egfavre.controllers;
 
 import org.h2.tools.Server;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.PostConstruct;
 import java.io.FileNotFoundException;
@@ -15,5 +17,11 @@ public class RestController {
     public void init() throws SQLException, FileNotFoundException {
         Server.createWebServer().start();
     }
+
+    @RequestMapping (path = "/", method = RequestMethod.GET)
+    public String displayWelcomePage (){
+        return "welcome";
+    }
+
 }
 
